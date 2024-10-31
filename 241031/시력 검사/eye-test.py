076ecc560637d12fcd587e84161ca106625e715f
2reten <145303952+2reten@users.noreply.github.com>
2024-10-31 10:36:08 +0900

    Title: 시력 검사, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/시력 검사/eye-test.py
+++ 241031/시력 검사/eye-test.py
@@ -3,6 +3,6 @@ vision_l = float(input())
 
 if vision_l >= 1.0 and vision_r >= 1.0:
     print("High")
-elif 1.0 > vision_l >= 0.5 and 1.0 > vision_r >= 0.5:
+elif vision_l >= 0.5 and vision_r >= 0.5:
     print("Middle")
 else: print("Low")
