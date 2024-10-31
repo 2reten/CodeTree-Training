1218ab5e529c9cc70e852950b19fc0d661f68a0a
2reten <145303952+2reten@users.noreply.github.com>
2024-10-31 10:32:07 +0900

    Title: 점수 비교, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/점수 비교/score-comparison.py
+++ 241031/점수 비교/score-comparison.py
@@ -1,5 +1,5 @@
-math1, math2 = list(map(int,input().split()))
-eng1, eng2 = list(map(int,input().split()))
+math1, eng1 = list(map(int,input().split()))
+math2, eng2 = list(map(int,input().split()))
 if math1 > math2 and eng1 > eng2:
     print(1)
 else: print(0)
