d07f1372264fbd6bcd3acc6612f01282f3cb2784
2reten <145303952+2reten@users.noreply.github.com>
2024-10-31 11:14:33 +0900

    Title: 좀 더 어려운 수학 점수, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 241031/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -3,4 +3,8 @@ math2, eng2 = list(map(int,input().split()))
 
 if math1 > math2:
     print("A")
+elif math1 == math2:
+    if eng1 > eng2:
+        print("A")
+    else: print("B")
 else: print("B")
