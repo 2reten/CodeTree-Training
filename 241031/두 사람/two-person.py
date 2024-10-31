0a67c34e4b2c00058c919294055fed517ae1dccb
2reten <145303952+2reten@users.noreply.github.com>
2024-10-31 11:18:12 +0900

    Title: 두 사람, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 241031/두 사람/two-person.py
+++ 241031/두 사람/two-person.py
@@ -1,8 +1,8 @@
 age1, sex1 = input().split()
 age2, sex2 = input().split()
 
-if age1 >= 20 and sex1 == "M":
+if int(age1) >= 20 and sex1 == "M":
     print(1)
-elif age2 >= 20 and sex2 == "M":
+elif int(age2) >= 20 and sex2 == "M":
     print(1)
 else: print(0)
