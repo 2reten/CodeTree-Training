5f420cadb3524220e6db5e93695b4eb4f061262d
2reten <145303952+2reten@users.noreply.github.com>
2024-10-31 11:16:42 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241031/두 사람/two-person.py
@@ -0,0 +1,12 @@
+age1, sex1 = input().split()
+age2, sex2 = input().split()
+
+if age1 >= 20:
+    if sex1 = "M":
+        print(1)
+    else: print(0)
+elif age2 >= 20:
+    if sex2 = "M":
+        print(1)
+    else: print(0)
+else: print(0)
