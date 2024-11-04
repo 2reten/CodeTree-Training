8b42f944de1a11c3c9686c12de162467c0631a6b
2reten <145303952+2reten@users.noreply.github.com>
2024-11-04 17:31:56 +0900

    Title: 최댓값 구하기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241104/최댓값 구하기/maximum-value.py
@@ -0,0 +1,11 @@
+a, b, c = list(map(int,input().split()))
+
+if a > b or a > c:
+    if a > b or a > c:
+        print(a)
+elif b > a or b > c:
+    if b > a and b > c:
+        print(b)
+elif c > a or c > a:
+    if c > a and c > b:
+        print(c)
